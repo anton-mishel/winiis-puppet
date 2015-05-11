17c6ad9f05610d4e1a413d9279c37985b7f63c37
root <root@puppet-test.nrgene.local>
2015-05-12 00:24:44 +0300

    Reorg module classes

--- modules/pruninstalliis/manifests/init.pp
+++ modules/pruninstalliis/manifests/init.pp
@@ -1,26 +1,4 @@
-class pruninstalliis { 
- windowsfeature { 'IIS':
-      feature_name => [
-        'Web-Server',
-        'Web-WebServer',
-        'Web-Basic-Auth',
-        'Web-Windows-Auth',
-        'Web-Asp',
-        'Web-Filtering',
-        'Web-ISAPI-Ext',
-        'Web-Mgmt-Console',
-        'Web-Default-Doc',
-        'Web-Dir-Browsing',
-        'Web-Http-Errors',
-        'Web-Http-Errors',
-        'Web-Static-Content',
-        'Web-Http-Logging',
-      ]
-    }
-      windowsfeature { 'Web-Stat-Compression':
-      ensure => absent,
-    }
-      windowsfeature { 'Web-Dyn-Compression':
-      ensure => absent,
-    }
+class pruninstalliis {
+ include pruninstalliis::install
+ include pruninstalliis::postinstall
 }
