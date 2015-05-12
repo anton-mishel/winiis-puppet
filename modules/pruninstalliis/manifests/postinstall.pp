40be368e3de6b08f0d267f002ea7ae09f1a1ac6c
root <anton.mishel@gmail.com>
2015-05-12 20:12:34 +0300

    Added post install stages

--- modules/pruninstalliis/manifests/postinstall.pp
+++ modules/pruninstalliis/manifests/postinstall.pp
@@ -7,4 +7,10 @@ windowsfeature { 'Web-Stat-Compression':
       ensure => absent,
       require => Class['pruninstalliis::install'],
     }
+exec { "Configure_iis_errors" :
+      command   => "Import-Module WebAdministration;Set-WebConfigurationProperty -pspath \'IIS:\\\' -filter system.webServer/httpErrors -name errorMode  -value Detailed ",
+      provider  => 'powershell',
+      logoutput => true,
+      require   => Class['pruninstalliis::install' ],
+    }
 }
