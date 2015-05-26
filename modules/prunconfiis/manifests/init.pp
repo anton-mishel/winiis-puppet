98e0ca9edabafd7d83eaedda46877f830af918a3
root <anton.mishel@gmail.com>
2015-05-26 18:16:19 +0300

    Added one more site for example

--- modules/prunconfiis/manifests/init.pp
+++ modules/prunconfiis/manifests/init.pp
@@ -7,4 +7,12 @@ class prunconfiis {
     iis_site_https_port => '8443',
     iis_certificate_thumbprint => 'cbc896e70edbe35b9b133a9c2ec1a2f5e2edaa59'
 }
+   prunconfiis::createsite {'cnn':
+    iis_site_name => 'cnn.com',
+    iis_host_header => 'www.cnn.com',
+    iis_site_path => 'c:\temp',
+    iis_site_http_port => '8888',
+    iis_site_https_port => '8453',
+    iis_certificate_thumbprint => 'cbc896e70edbe35b9b133a9c2ec1a2f5e2edaa59'
+}
 }
