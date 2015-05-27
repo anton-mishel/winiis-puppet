02743f2571800c1a63e4f729830a64a23b567ee7
root <anton.mishel@gmail.com>
2015-05-27 17:35:19 +0300

    Fixing ISAPI BUG

--- modules/pruninstalliis/manifests/postinstall.pp
+++ modules/pruninstalliis/manifests/postinstall.pp
@@ -15,7 +15,9 @@ exec { "Configure_iis_errors" :
       require   => Class['pruninstalliis::install' ],
     }
 exec { "isapi-restrictions" :
-      command => "Add-WebConfiguration -pspath 'MACHINE/WEBROOT/APPHOST' -filter 'system.webServer/security/isapiCgiRestriction' -value @{\ndescription = 'PRUN'\npath        = 'C:\inetpub\scripts\prun.dll'\n    allowed     = 'True'\n}",
+      #command => "Add-WebConfiguration -pspath 'IIS:\' -filter 'system.webServer/security/isapiCgiRestriction' -value @{\ndescription = 'PRUN'\npath        = 'C:\inetpub\scripts\prun.dll'\n    allowed     = 'True'\n}",
+      command => "Add-WebConfiguration -pspath 'IIS:\' -filter 'system.webServer/security/isapiCgiRestriction' -value @{description='PRUN';path='C:\inetpub\scripts\prun.dll';allowed='True'}",
+      #command => "Add-WebConfiguration -pspath 'IIS:\' -filter 'system.webServer/security/isapiCgiRestriction' -value @{path='C:\inetpub\scripts\prun.dll'}",
       provider  => 'powershell',
       logoutput => true,
       require   => Class['pruninstalliis::install' ],
